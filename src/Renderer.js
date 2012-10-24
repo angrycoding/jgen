000d0e066d189410c3f4286840860c5d7cb33ac9
Ruslan Matveev <matveev.ruslan@gmail.com>
2012-10-24 12:30:21 +0400

    added layers functionality

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,10 +1,11 @@
 var Renderer = Class(function() {
 
 	var instanceID = null;
+
 	var viewPort = null;
-	var mapViewPort = null;
 	var tileElement = null;
 	var styleElement = null;
+	var tileViewPort = null;
 	var spriteViewPort = null;
 
 	var mapWidth = 0, mapHeight = 0;
@@ -12,13 +13,11 @@ var Renderer = Class(function() {
 	var maxScrollX = 0, maxScrollY = 0;
 	var viewPortWidth = 0, viewPortHeight = 0;
 
-
-
+	var layersCount = 0, layersData = [];
 	var cameraWidth = 0, cameraHeight = 0;
 
 	var tileCache = {};
 	var tileDefinitions = {};
-	var mapData = [];
 
 	function recalcVariables() {
 		maxScrollX = Math.max(0, tileWidth * mapWidth - viewPortWidth);
@@ -27,25 +26,24 @@ var Renderer = Class(function() {
 
 	function constructor() {
 		instanceID = new Date().getTime().toString(16);
-		tileElement = document.createElement('div');
-		styleElement = document.createElement('style');
-		styleElement.setAttribute('type', 'text/css');
-		document.head.appendChild(styleElement);
 		viewPort = document.createElement('div');
 		viewPort.id = ('jgen-' + instanceID);
 		viewPort.style.position = 'relative';
 		viewPort.style.overflow = 'hidden';
-		mapViewPort = document.createElement('div');
-		mapViewPort.style.position = 'absolute';
+
+		tileViewPort = document.createElement('div');
+		tileViewPort.style.position = 'absolute';
+		viewPort.appendChild(tileViewPort);
+
 		spriteViewPort = document.createElement('div');
 		spriteViewPort.style.position = 'absolute';
-		viewPort.appendChild(mapViewPort);
 		viewPort.appendChild(spriteViewPort);
-	}
 
-	function setCamera(width, height) {
-		cameraWidth = width;
-		cameraHeight = height;
+		tileElement = document.createElement('div');
+		tileElement.style.opacity =1;
+		styleElement = document.createElement('style');
+		styleElement.setAttribute('type', 'text/css');
+		document.head.appendChild(styleElement);
 	}
 
 	function setMapSize(width, height) {
@@ -65,17 +63,23 @@ var Renderer = Class(function() {
 		viewPortHeight = height;
 		viewPort.style.width = width + 'px';
 		viewPort.style.height = height + 'px';
-		mapViewPort.style.width = width + 'px';
-		mapViewPort.style.height = height + 'px';
+		tileViewPort.style.width = width + 'px';
+		tileViewPort.style.height = height + 'px';
 		spriteViewPort.style.width = width + 'px';
 		spriteViewPort.style.height = height + 'px';
 		target.appendChild(viewPort);
 		recalcVariables();
 	}
 
-	function setTileDefinition(tileID, tileWidth, tileHeight, tileUrl) {
+	function setTileDefinition(tileID, tileWidth, tileHeight, tileUrl, offsetX, offsetY) {
 		var css = [];
-		tileDefinitions[tileID] = [tileWidth, tileHeight, tileUrl];
+		tileDefinitions[tileID] = [
+			tileWidth,
+			tileHeight,
+			tileUrl,
+			offsetX || 0,
+			offsetY || 0
+		];
 		for (var tileID in tileDefinitions) {
 			var tileDefinition = tileDefinitions[tileID];
 			css.push('#jgen-' + instanceID);
@@ -83,55 +87,60 @@ var Renderer = Class(function() {
 			css.push('position: absolute;');
 			css.push('width: ' + tileDefinition[0] + 'px;');
 			css.push('height: ' + tileDefinition[1] + 'px;');
-
-			css.push('margin-top: ' + (64 - tileDefinition[1])  + 'px;');
-			css.push('margin-left: ' + (64 - tileDefinition[0]) / 2  + 'px;');
-
-			css.push('background-position: center bottom;');
 			css.push('background-repeat: no-repeat;');
 			css.push('background-image: url("' + tileDefinition[2] + '");')
+			css.push('background-position: -' + tileDefinition[3] + 'px -' + tileDefinition[4] + 'px;');
 			css.push('}');
 		}
 		styleElement.innerHTML = css.join('\n');
 	}
 
 	function render(scrollX, scrollY) {
-		var row, col, tile, tileID, cacheKey;
+		var layer, row, col, tile, tileID, cacheKey;
 		var marginLeft = (scrollX % tileWidth);
 		var marginTop = (scrollY % tileHeight);
-		mapViewPort.style.left = -marginLeft + 'px';
-		mapViewPort.style.top = -marginTop + 'px';
+		tileViewPort.style.left = -marginLeft + 'px';
+		tileViewPort.style.top = -marginTop + 'px';
 		var tileX = Math.floor(scrollX / tileWidth);
 		var tileY = Math.floor(scrollY / tileHeight);
 		var tileW = Math.ceil((viewPortWidth + marginLeft) / tileWidth);
 		var tileH = Math.ceil((viewPortHeight + marginTop) / tileHeight);
-		for (row = 0; row < tileH; row++) {
-			for (col = 0; col < tileW; col++) {
-				cacheKey = ((col << 16) | row);
-				if (!tileCache.hasOwnProperty(cacheKey)) {
-					tile = tileElement.cloneNode(false);
-					tile.style.left = (tileWidth * col) + 'px';
-					tile.style.top = (tileHeight * row) + 'px';
-					// tile.style.zIndex = 30 - row;
-					mapViewPort.appendChild(tile);
-					tileCache[cacheKey] = tile;
-				} else tile = tileCache[cacheKey];
-				tileID = mapData[tileY + row][tileX + col];
-				tile.className = ('tile-' + tileID);
+
+		for (layer = 0; layer < layersCount; layer++) {
+
+			for (row = 0; row < tileH; row++) {
+				for (col = 0; col < tileW; col++) {
+					cacheKey = (layer * 31 + row) * 31 + col;
+					if (!tileCache.hasOwnProperty(cacheKey)) {
+						tile = tileElement.cloneNode(false);
+						tile.style.left = (tileWidth * col) + 'px';
+						tile.style.top = (tileHeight * row) + 'px';
+						tile.style.zIndex = (layer * 2);
+						tileViewPort.appendChild(tile);
+						tileCache[cacheKey] = tile;
+					} else tile = tileCache[cacheKey];
+					tileID = layersData[layer][tileY + row][tileX + col];
+					tile.className = ('tile-' + tileID);
+				}
 			}
+
 		}
 	}
 
-	function setMapData(data) {
-		mapData = data;
+	function setCamera(width, height) {
+		cameraWidth = width;
+		cameraHeight = height;
 	}
 
-	var sprites = [];
+	function addLayer(layerData) {
+		layersData.push(layerData);
+		layersCount = layersData.length;
+	}
 
 	return {
 
 		constructor: constructor,
-		setMapData: setMapData,
+		addLayer: addLayer,
 		setMapSize: setMapSize,
 		setTileSize: setTileSize,
 		setViewPort: setViewPort,
@@ -139,12 +148,8 @@ var Renderer = Class(function() {
 		setTileDefinition: setTileDefinition,
 
 		addSprite: function(sprite) {
-			sprites.push(sprite);
-			spriteViewPort.appendChild(sprite.getView());
-		},
-
-		getCamera: function() {
-			return Math.random();
+			var spriteView = sprite.getView();
+			spriteViewPort.appendChild(spriteView);
 		},
 
 		render: function(scrollX, scrollY) {

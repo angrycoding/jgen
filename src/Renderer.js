e1464773b455b6115b553fb141b69bf99a024ddb
Ruslan <matveev.ruslan@gmail.com>
2012-12-24 23:51:26 +0400

    Sprite & Timer + get rid of jsface

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,45 +1,36 @@
-var Renderer = Class(function() {
-
-	var instanceID = null;
-
-	var viewPort = null;
-	var tileElement = null;
-	var styleElement = null;
-	var tileViewPort = null;
-	var spriteViewPort = null;
-	var tileElementCache = {};
+var Renderer = (function() {
 
 	var mapWidth = 0, mapHeight = 0;
 	var tileWidth = 0, tileHeight = 0;
 	var maxScrollX = 0, maxScrollY = 0;
 	var viewPortWidth = 0, viewPortHeight = 0;
 
+	var tileCache = {}, tiles = {};
 	var layersCount = 0, layersData = [];
-	var cameraWidth = 0, cameraHeight = 0;
 
-	var tileDefinitions = {};
+	var instanceID = new Date().getTime().toString(16);
+
+	var viewPort = document.createElement('div');
+	viewPort.style.position = 'relative';
+	viewPort.style.overflow = 'hidden';
+
+	var tileViewPort = document.createElement('div');
+	tileViewPort.id = ('jgen-tiles-' + instanceID);
+	tileViewPort.style.position = 'absolute';
+	viewPort.appendChild(tileViewPort);
+
+	var spriteViewPort = document.createElement('div');
+	spriteViewPort.style.position = 'absolute';
+	viewPort.appendChild(spriteViewPort);
+
+	var tileElement = document.createElement('div');
+	var styleElement = document.createElement('style');
+	styleElement.setAttribute('type', 'text/css');
+	document.head.appendChild(styleElement);
 
 	function recalcVariables() {
 		maxScrollX = Math.max(0, tileWidth * mapWidth - viewPortWidth);
 		maxScrollY = Math.max(0, tileHeight * mapHeight - viewPortHeight);
-	}
-
-	function constructor() {
-		instanceID = new Date().getTime().toString(16);
-		viewPort = document.createElement('div');
-		viewPort.style.position = 'relative';
-		viewPort.style.overflow = 'hidden';
-		tileViewPort = document.createElement('div');
-		tileViewPort.id = ('jgen-tiles-' + instanceID);
-		tileViewPort.style.position = 'absolute';
-		viewPort.appendChild(tileViewPort);
-		spriteViewPort = document.createElement('div');
-		spriteViewPort.style.position = 'absolute';
-		viewPort.appendChild(spriteViewPort);
-		tileElement = document.createElement('div');
-		styleElement = document.createElement('style');
-		styleElement.setAttribute('type', 'text/css');
-		document.head.appendChild(styleElement);
 	}
 
 	function setMapSize(width, height) {
@@ -52,6 +43,11 @@ var Renderer = Class(function() {
 		tileWidth = width;
 		tileHeight = height;
 		recalcVariables();
+	}
+
+	function addLayer(layerData) {
+		layersData.push(layerData);
+		layersCount = layersData.length;
 	}
 
 	function setViewPort(target, width, height) {
@@ -69,9 +65,9 @@ var Renderer = Class(function() {
 
 	function setTileDefinition(tileID, tileUrl, offsetX, offsetY) {
 		var css = [];
-		tileDefinitions[tileID] = [tileUrl, offsetX || 0, offsetY || 0];
-		for (var tileID in tileDefinitions) {
-			var tileDefinition = tileDefinitions[tileID];
+		tiles[tileID] = [tileUrl, offsetX || 0, offsetY || 0];
+		for (var tileID in tiles) {
+			var tileDefinition = tiles[tileID];
 			css.push('#jgen-tiles-' + instanceID);
 			css.push('.tile-' + tileID + '{');
 			css.push('position: absolute;');
@@ -85,13 +81,21 @@ var Renderer = Class(function() {
 		styleElement.innerHTML = css.join('\n');
 	}
 
-	function render(scrollX, scrollY) {
+	function renderTiles(scrollX, scrollY) {
+
+		if (scrollX < 0) scrollX = 0;
+		if (scrollY < 0) scrollY = 0;
+		if (scrollX > maxScrollX) scrollX = maxScrollX;
+		if (scrollY > maxScrollY) scrollY = maxScrollY;
+
 		var rowData, cellData, tileID;
 		var layer, row, col, tile, cacheKey;
 		var marginLeft = (scrollX % tileWidth);
 		var marginTop = (scrollY % tileHeight);
+
 		tileViewPort.style.left = -marginLeft + 'px';
 		tileViewPort.style.top = -marginTop + 'px';
+
 		var tileX = Math.floor(scrollX / tileWidth);
 		var tileY = Math.floor(scrollY / tileHeight);
 		var tileW = Math.ceil((viewPortWidth + marginLeft) / tileWidth);
@@ -100,17 +104,18 @@ var Renderer = Class(function() {
 			rowData = layersData[layer];
 			for (row = 0; row < tileH; row++) {
 				cellData = rowData[tileY + row];
+				if (!cellData) continue;
 				for (col = 0; col < tileW; col++) {
 					tileID = cellData[tileX + col];
 					cacheKey = (layer * 31 + row) * 31 + col;
-					if (!(tile = tileElementCache[cacheKey])) {
+					if (!(tile = tileCache[cacheKey])) {
 						tile = tileElement.cloneNode(false);
 						tile.style.zIndex = (layer * 2);
 						tile.className = ('tile-' + tileID);
 						tile.style.left = (tileWidth * col) + 'px';
 						tile.style.top = (tileHeight * row) + 'px';
 						tileViewPort.appendChild(tile);
-						tile = tileElementCache[cacheKey] = [tile, tileID];
+						tile = tileCache[cacheKey] = [tile, tileID];
 					} else if (tile[1] !== tileID) {
 						tile[1] = tileID;
 						tile[0].className = ('tile-' + tileID);
@@ -118,61 +123,22 @@ var Renderer = Class(function() {
 				}
 			}
 		}
-	}
 
-	function setCamera(width, height) {
-		cameraWidth = width;
-		cameraHeight = height;
-	}
-
-	function addLayer(layerData) {
-		layersData.push(layerData);
-		layersCount = layersData.length;
+		return [scrollX, scrollY];
 	}
 
 	return {
 
-		constructor: constructor,
 		addLayer: addLayer,
+		render: renderTiles,
 		setMapSize: setMapSize,
 		setTileSize: setTileSize,
 		setViewPort: setViewPort,
-		setCamera: setCamera,
 		setTileDefinition: setTileDefinition,
 
 		addSprite: function(sprite) {
 			var spriteView = sprite.getView();
 			spriteViewPort.appendChild(spriteView);
-		},
-
-		render: function(scrollX, scrollY) {
-
-			var cWidth = cameraWidth;
-			var cHeight = cameraHeight;
-
-			if (cWidth < 0) cWidth = (viewPortWidth + cWidth);
-			if (cHeight < 0) cHeight = (viewPortHeight + cHeight);
-
-			// return render(scrollX, scrollY);
-
-			var aaa = (scrollX - cWidth);
-			var bbb = (scrollY - cHeight);
-
-			render(
-				Math.max(0, Math.min(aaa, maxScrollX)),
-				Math.max(0, Math.min(bbb, maxScrollY))
-			);
-
-			return [
-				Math.max(
-					Math.min(scrollX, cWidth),
-					cWidth + (aaa - maxScrollX)
-				),
-				Math.max(
-					Math.min(scrollY, cHeight),
-					cHeight + (bbb - maxScrollY)
-				)
-			];
 		}
 
 	}
--- src/Sprite.js
+++ src/Sprite.js
@@ -1,27 +1,204 @@
-var Sprite = Class(function() {
+var Sprite = (function() {
 
-	var spriteEl = null;
+	var sprite = document.createElement('div');
+	sprite.style.display = 'none';
+	sprite.style.position = 'absolute';
+	sprite.style.backgroundPosition = '0px 0px';
+	sprite.style.backgroundRepeat = 'no-repeat';
 
-	return {
+	return function(width, height, url, states, handlers, cManager) {
 
-		constructor: function(width, height) {
-			spriteEl = document.createElement('div');
-			spriteEl.style.opacity = 0.7;
-			spriteEl.style.width = width + 'px';
-			spriteEl.style.height = height + 'px';
-			spriteEl.style.backgroundColor = 'red';
-			spriteEl.style.position = 'absolute';
-		},
+		var frameNumber = 0;
+		var frameBuffer = [];
 
-		setPosition: function(x, y) {
-			spriteEl.style.left = x + 'px';
-			spriteEl.style.top = y + 'px';
-		},
+		var spriteX = 0;
+		var spriteY = 0;
+		var spriteState = {};
+		var spriteFrame = null;
+		var spriteRotation = null;
+		var spriteVisible = false;
 
-		getView: function() {
-			return spriteEl;
+		var frameChanged = false;
+		var stateChanged = false;
+		var rotationChanged = false;
+		var positionChanged = false;
+		var visibilityChanged = false;
+
+		var spriteEl = sprite.cloneNode(true);
+		spriteEl.style.width = width + 'px';
+		spriteEl.style.height = height + 'px';
+		spriteEl.style.backgroundImage = 'url("'+url+'")';
+
+		if (!(handlers instanceof Object) ||
+			handlers instanceof Array) {
+			handlers = {};
 		}
+
+		var onRotate = handlers.onRotate;
+		if (!onRotate instanceof Function) onRotate = null;
+
+		var onStateChange = handlers.onStateChange;
+		if (!onStateChange instanceof Function) onStateChange = null;
+
+		function setPosition(x, y) {
+			var collision = cManager.adjust(x, y, width, height);
+
+			if (collision) {
+
+				if (instance.y + height <= collision[1]) {
+					y = collision[1] - height;
+				} else if (instance.y >= collision[1] + collision[3]) {
+					y = collision[1] + collision[3];
+				}
+
+				if (instance.x + width <= collision[0]) {
+					x = collision[0] - width;
+				} else if (instance.x >= collision[0] + collision[2]) {
+					x = collision[0] + collision[2];
+				}
+			}
+
+			var isMoved = (instance.x !== x || instance.y !== y);
+
+			instance.x = x;
+			instance.y = y;
+
+
+			return isMoved;
+		}
+
+		return {
+
+			getView: function() {
+				return spriteEl;
+			},
+
+			getPosition: function() {
+				return [spriteX, spriteY];
+			},
+
+			getCenter: function() {
+				return [
+					spriteX + width / 2,
+					spriteY + height / 2
+				];
+			},
+
+			getWidth: function() {
+				return width;
+			},
+
+			getHeight: function() {
+				return height;
+			},
+
+			move: function(x, y) {
+				if (spriteX !== x ||
+					spriteY !== y) {
+					spriteX = x;
+					spriteY = y;
+					positionChanged = true;
+				}
+				return true;
+			},
+
+			show: function() {
+				if (!spriteVisible) {
+					spriteVisible = true;
+					visibilityChanged = true;
+				}
+			},
+
+			hide: function() {
+				if (spriteVisible) {
+					spriteVisible = false;
+					visibilityChanged = true;
+				}
+			},
+
+			rotate: function(rotation) {
+				if (spriteRotation !== rotation) {
+					spriteRotation = rotation;
+					rotationChanged = true;
+				}
+			},
+
+			walk: function(angle, speed) {
+				this.rotate(angle);
+				return this.move(
+					spriteX + Math.cos(angle) * speed,
+					spriteY + Math.sin(angle) * speed
+				);
+			},
+
+			setState: function(state, value) {
+				if (spriteState[state] !== value) {
+					spriteState[state] = value;
+					stateChanged = true;
+				}
+			},
+
+			setFrame: function(frame) {
+				if (spriteFrame !== frame) {
+					frameNumber = 0;
+					spriteFrame = frame;
+					frameChanged = true;
+				}
+			},
+
+			render: function(scrollX, scrollY) {
+
+				if (visibilityChanged) {
+					visibilityChanged = false;
+					if (spriteVisible) {
+						spriteEl.style.display = 'block';
+					} else spriteEl.style.display = 'none';
+				}
+
+				if (rotationChanged) {
+					rotationChanged = false;
+					if (onRotate) {
+						onRotate.call(
+							this,
+							spriteRotation
+						);
+					}
+				}
+
+				if (stateChanged) {
+					stateChanged = false;
+					if (onStateChange) {
+						onStateChange.call(
+							this,
+							spriteState
+						);
+					}
+				}
+
+				if (frameChanged) {
+					frameChanged = false;
+					frameBuffer = states[spriteFrame];
+				}
+
+				if (positionChanged) {
+					positionChanged = false;
+					// spriteEl.style.webkitTransform = 'translate(' + (spriteX - scrollX || 0) + 'px,' + (spriteY - scrollY || 0) + 'px)';
+					spriteEl.style.left = (spriteX - scrollX || 0) + 'px';
+					spriteEl.style.top = (spriteY - scrollY || 0) + 'px';
+				}
+
+				// spriteEl.style.zIndex = spriteY;
+				spriteEl.style.backgroundPosition = ('-' + (
+					frameBuffer[frameNumber] ||
+					frameBuffer[frameNumber = 0]
+				).join('px -') + 'px');
+
+				frameNumber++;
+
+			}
+
+		};
 
 	};
 
-});
+})();
